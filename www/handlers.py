287a0c198674d7c720a99aea35eedd2c4daf7601
Zhang Yuang <1147988774@qq.com>
2022-03-23 16:30:38 +0800

    day-9 Build the API

--- www/handlers.py
+++ www/handlers.py
@@ -18,3 +18,10 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.find_all(order_by='CREATED_AT DESC')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
